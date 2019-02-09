bcce81205cee69b5727a96fdbde663d006c4de84
harshit57709 <chaudharyharshit0@gmail.com>
2019-02-09 15:27:02 +0530

    python code added

---
+++ arduino.py
@@ -0,0 +1,9 @@
+import serial
+import pyautogui
+arduino=serial.Serial('COM3',9600)
+while 1:
+	incoming_Data=arduino.readline()
+	if 'up' in incoming_Data.decode('utf-8'):
+		pyautogui.press('up')
+		
+	incoming_Data=""						
